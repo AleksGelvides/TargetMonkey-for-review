d33a01ccd18d1e9da0de6ea544da43efe9969153
AlekseyGelvides <Alexey_Gelvides@epam.com>
2022-02-18 17:47:32 +0700

    Добавил игнорирование полей в маппер

--- customer-service/customer-service-impl/src/main/java/com/targetmonkey/registrationserviceimpl/mappers/CompanyMapper.java
+++ customer-service/customer-service-impl/src/main/java/com/targetmonkey/registrationserviceimpl/mappers/CompanyMapper.java
@@ -4,6 +4,7 @@ import com.targetmonkey.registrationserviceapi.dto.companies.CompanyAdminDto;
 import com.targetmonkey.registrationserviceapi.dto.companies.CompanyUserDto;
 import com.targetmonkey.registrationserviceimpl.domain.CompanyJpa;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -19,8 +20,18 @@ public interface CompanyMapper {
 
     CompanyUserDto toCompanyUserDto(CompanyAdminDto companyAdminDto); // Из Админа в Кастомера
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ownerId", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "leadsCounts", ignore = true)
     void updateCompanyAdminDTO(CompanyAdminDto newCompany, @MappingTarget CompanyAdminDto oldCompany); //Обновление: Админ/Админ
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ownerId", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "leadsCounts", ignore = true)
     void updateCompanyUserDTO(CompanyUserDto newCompany, @MappingTarget CompanyAdminDto oldCompany);   //Обновление: Кастомер/Админ
 
 
