14b472deb02bb4100a9a44b4f05d2af30540adf2
AlekseyGelvides <Alexey_Gelvides@epam.com>
2022-03-01 17:03:38 +0700

    Готовы тесты CustomerServiceImpl

--- authentication-service/authentication-service-impl/src/main/java/com/targetmonkey/authenticationserviceimpl/service/CustomerServiceImpl.java
+++ authentication-service/authentication-service-impl/src/main/java/com/targetmonkey/authenticationserviceimpl/service/CustomerServiceImpl.java
@@ -56,7 +56,6 @@ public class CustomerServiceImpl implements CustomerService, UserDetailsService 
     @Override
     @SneakyThrows
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         try {
             var customerJpa = customerRepository.findByUsername(username);
             List<SimpleGrantedAuthority> authorities = customerJpa.getRoles().stream()
@@ -66,6 +65,5 @@ public class CustomerServiceImpl implements CustomerService, UserDetailsService 
         }catch (NullPointerException e){
             throw new UsernameNotFoundException("This user not found");
         }
-
     }
 }
